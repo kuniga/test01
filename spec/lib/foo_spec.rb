caa319867f025d7184bfcf841fb2613c93b0da5a
Yoshinori Kuniga <kuniga+github@gmail.com>
2015-09-04 14:51:07 +0900

    Update foo_spec.rb

--- spec/lib/foo_spec.rb
+++ spec/lib/foo_spec.rb
@@ -9,8 +9,18 @@ describe Foo do
     expect(foo.respond_to? :add)
   end
 
+  it "has method 'subtract'" do
+    foo = Foo.new
+    expect(foo.respond_to? :subtract)
+  end
+
   it "2 たす 3 は 5" do
     foo = Foo.new
     expect(foo.add(2, 3)).to equal(5)
+  end
+
+  it "8 ひく 4 は 4" do
+    foo = Foo.new
+    expect(foo.subtract(8, 4)).to equal(4)
   end
 end
